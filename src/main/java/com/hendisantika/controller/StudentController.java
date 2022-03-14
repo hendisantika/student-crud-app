2b36a59d46e96fa49be545fc2b9792497621c5df
Hendi Santika <hendisantika@gmail.com>
2022-03-15 06:33:28 +0700

    #18 - Add addStudent API in StudentController

--- src/main/java/com/hendisantika/controller/StudentController.java
+++ src/main/java/com/hendisantika/controller/StudentController.java
@@ -6,9 +6,12 @@ import com.hendisantika.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import java.util.List;
 
@@ -41,5 +44,10 @@ public class StudentController {
         Student std = studentservice.findById(id)
                 .orElseThrow(() -> new StudentNotFoundException("Student with " + id + " is Not Found!"));
         return std;
+    }
+
+    @PostMapping
+    public Student addStudent(@Valid @RequestBody Student std) {
+        return studentservice.save(std);
     }
 }
