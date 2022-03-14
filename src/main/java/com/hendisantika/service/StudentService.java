4037515b0da9a22f960bfe165c6540c6f00b8eb6
Hendi Santika <hendisantika@gmail.com>
2022-03-15 06:23:46 +0700

    #6 - Add Student Service

---
+++ src/main/java/com/hendisantika/service/StudentService.java
@@ -0,0 +1,27 @@
+package com.hendisantika.service;
+
+import com.hendisantika.entity.Student;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : student-crud-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 15/03/22
+ * Time: 06.23
+ */
+public interface StudentService {
+    List<Student> getAllStudents();
+
+    Optional<Student> findById(int id);
+
+    Optional<Student> findByEmail(String email);
+
+    Student save(Student std);
+
+    void deleteById(int id);
+}
