a6fe808483600cac0dd8e08e0ee5bc4a109a7fc1
Hendi Santika <hendisantika@gmail.com>
2022-03-15 06:33:06 +0700

    #17 - Add getStudentById API in StudentController

--- src/main/java/com/hendisantika/controller/StudentController.java
+++ src/main/java/com/hendisantika/controller/StudentController.java
@@ -1,12 +1,15 @@
 package com.hendisantika.controller;
 
 import com.hendisantika.entity.Student;
+import com.hendisantika.exception.StudentNotFoundException;
 import com.hendisantika.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.constraints.Min;
 import java.util.List;
 
 /**
@@ -19,7 +22,7 @@ import java.util.List;
  * Time: 06.31
  */
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/students")
 public class StudentController {
     private final StudentService studentservice;
 
@@ -28,8 +31,15 @@ public class StudentController {
         this.studentservice = studentservice;
     }
 
-    @GetMapping(value = "/students")
+    @GetMapping
     public List<Student> getAllStudents() {
         return studentservice.getAllStudents();
+    }
+
+    @GetMapping(value = "/{id}")
+    public Student getStudentById(@PathVariable("id") @Min(1) Long id) {
+        Student std = studentservice.findById(id)
+                .orElseThrow(() -> new StudentNotFoundException("Student with " + id + " is Not Found!"));
+        return std;
     }
 }
