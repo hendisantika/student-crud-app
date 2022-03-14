757c78c9eb1a21ade2907909fea622b73d39d888
Hendi Santika <hendisantika@gmail.com>
2022-03-15 06:27:50 +0700

    #8 - Add Student Service Impl

---
+++ src/main/java/com/hendisantika/service/StudentServiceImpl.java
@@ -0,0 +1,26 @@
+package com.hendisantika.service;
+
+import com.hendisantika.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : student-crud-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 15/03/22
+ * Time: 06.24
+ */
+@Service
+public class StudentServiceImpl implements StudentService {
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+
+}
