441a8b5df0e91bffb23338d104dcb448b0884aaf
Hendi Santika <hendisantika@gmail.com>
2022-03-15 06:32:20 +0700

    #16 - Add getAllStudents API in StudentController

--- src/main/java/com/hendisantika/controller/StudentController.java
+++ src/main/java/com/hendisantika/controller/StudentController.java
@@ -1,9 +1,13 @@
 package com.hendisantika.controller;
 
+import com.hendisantika.entity.Student;
 import com.hendisantika.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,5 +26,10 @@ public class StudentController {
     @Autowired
     public StudentController(StudentService studentservice) {
         this.studentservice = studentservice;
+    }
+
+    @GetMapping(value = "/students")
+    public List<Student> getAllStudents() {
+        return studentservice.getAllStudents();
     }
 }
