b8351b6a606b997bd4ca7d4c3b04866468022b7c
Hendi Santika <hendisantika@gmail.com>
2022-05-17 17:32:28 +0700

    #48 - Add SpringDoc Beans

--- src/main/java/com/hendisantika/StudentCrudAppApplication.java
+++ src/main/java/com/hendisantika/StudentCrudAppApplication.java
@@ -1,7 +1,13 @@
 package com.hendisantika;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StudentCrudAppApplication {
@@ -10,4 +16,24 @@ public class StudentCrudAppApplication {
         SpringApplication.run(StudentCrudAppApplication.class, args);
     }
 
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("student")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String appDescription, @Value("${application" +
+            "-version}") String appVersion) {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Student CRUD Application REST API")
+                        .version(appVersion)
+                        .description(appDescription)
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+
+    }
 }
